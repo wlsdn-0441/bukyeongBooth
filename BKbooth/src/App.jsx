93b9e11c72593592ed21a72665916137349a7cc2
J <0426jinwoo@gmail.com>
2025-12-18 23:13:11 +0900

    Initial commit

---
+++ BKbooth/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1>Welcome to BKbooth</h1>
+    </div>
+  )
+}
+
+export default App
